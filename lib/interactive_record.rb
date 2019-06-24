0cb4deb4290678f155f3ee24501d49ad7ece3ac9
Jared Grove <jaredkgrove@gmail.com>
2019-06-24 19:18:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def initialize(options = {})
+
+  end
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
