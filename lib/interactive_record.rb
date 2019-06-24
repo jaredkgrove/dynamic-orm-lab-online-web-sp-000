09a33983e524355a1cfcff64a104375cae91e7de
Jared Grove <jaredkgrove@gmail.com>
2019-06-24 19:21:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,6 +3,9 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
+  def table_name_for_insert
+    self.class.table_name
+  end
   def initialize(options = {})
     options.each do |property, value|
       self.send("#{property}=", value)
